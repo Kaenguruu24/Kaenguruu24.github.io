98852ef8cc796007f96896fe9b8ef2100a7879c1
KaenguruuDev <user.jakob@outlook.de>
2024-02-19 16:18:44 +0100

    Stuff

--- index.js
+++ index.js
@@ -1,3 +1,63 @@
-window.onload = function () {
-	console.log("HELLO WORLD!");
-}
+fetch('assets/project_data.json')
+	.then(response => response.json())
+	.then(data => {
+		data["projects"].forEach(item => {
+			let element = document.getElementById('projects-container');
+
+			let html = `
+			<div class="rectangle">
+                <div class="project-title">
+                    <img src="${item.logo}" alt="Logo" />
+                    <h2>${item.name}</h2>
+                </div>
+                <div class="description">
+                    ${item.description}
+                </div>
+				`;
+			let indev_status = `
+                <div class="home-project-status indev">
+                    <h3>InDev</h3>
+                </div>
+				`;
+			let paused_status = `
+                <div class="home-project-status paused">
+                    <h3>Paused</h3>
+                </div>
+				`;
+			let cancelled_status = `
+                <div class="home-project-status cancelled">
+                    <h3>Cancelled</h3>
+                </div>
+				`;
+			let released_status = `
+                <div class="home-project-status released">
+                    <h3>Released</h3>
+                </div>
+				`;
+			let footer = `
+				<div class="button_container" >
+					<a href="${item.github_repo}">
+						<button class="button">Github</button>
+					</a>
+					<a href="projects.html#${item.name}">
+						<button class="button">More</button>
+					</a>
+				</div >
+			</div >
+			`;
+
+			if (item["status"] == "in_dev") {
+				html += indev_status;
+			} else if (item["status"] == "paused") {
+				html += paused_status;
+			} else if (item["status"] == "cancelled") {
+				html += cancelled_status;
+			} else if (item["status"] == "released") {
+				html += released_status;
+			}
+			html += footer;
+
+			element.insertAdjacentHTML('beforeend', html);
+		});
+	})
+	.catch(error => console.error('Error:', error));
--- projects.js
+++ projects.js
@@ -1,8 +1,79 @@
-document.getElementById('toggle-view').addEventListener('click', function () {
-	document.getElementById('project1').classList.toggle('resized');
-	document.getElementById('project1-buttons').classList.toggle('resized');
-	if (document.getElementById('project1').classList.contains('resized')) { document.getElementById('toggle-view').textContent = "Less"; }
-	else { document.getElementById('toggle-view').textContent = "More"; }
-	document.write("Hello World");
-	console.log("HEY HO");
-});
+var hashValue = window.location.hash.substring(1).replace("%20", " ");
+
+fetch('assets/project_data.json')
+	.then(response => response.json())
+	.then(data => {
+		jsonData = data;
+		data["projects"].forEach(item => {
+			let element = document.getElementById('projects-container');
+
+			let other = `
+			<div class="left_button_container" id="project${data["projects"].indexOf(item)}-buttons">
+			<a href="${item.github_repo}">
+				<button class="button">Github</button>
+			</a>
+			<a href="projects.html#${item.name}">
+				<button class="button" id="toggle-view" > More</button >
+			</a >
+		</div > `;
+
+			let head = ``;
+			if (hashValue == item.name) {
+				head = `<div class="vertical-rectangle resized" id="project${data["projects"].indexOf(item)}" >`;
+			} else {
+				head = `<div class="vertical-rectangle" id="project${data["projects"].indexOf(item)}" >`;
+			}
+			let html = `
+				<div class="project-title">
+					<img src="${item.logo}" alt="Logo">
+						<h2>${item.name}</h2>
+				</div>
+
+				<div class="project-description">
+					${item.description}
+				</div>
+			`;
+			let indev_status = `
+				<div class="home-project-status extended indev" >
+					<h3>In Development</h3>
+				</div >
+				`;
+			let paused_status = `
+				<div class="home-project-status extended paused" >
+					<h3>Paused</h3>
+				</div >
+				`;
+			let cancelled_status = `
+				<div class="home-project-status extended cancelled" >
+					<h3>Cancelled</h3>
+                </div >
+				`;
+			let released_status = `
+				<div class="home-project-status extended released" >
+					<h3>Released</h3>
+                </div >
+				`;
+			let footer = `
+			</div >
+				`;
+
+			head += html += other;
+
+			if (item["status"] == "in_dev") {
+				html += indev_status;
+			} else if (item["status"] == "paused") {
+				html += paused_status;
+			} else if (item["status"] == "cancelled") {
+				html += cancelled_status;
+			} else if (item["status"] == "released") {
+				html += released_status;
+			}
+			head += footer;
+
+			element.insertAdjacentHTML('beforeend', head);
+			document.getElementById(`project${data["projects"].indexOf(item)}-buttons`).addEventListener("click", function () {
+				document.getElementById(`project${data["projects"].indexOf(item)}`).classList.toggle("resized");
+			});
+		})
+	})
+	.catch(error => console.error('Error:', error));
